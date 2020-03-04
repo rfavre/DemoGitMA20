308a60e77ad4c244f3c2c5e2132968320f93e831
FAVRE Raphael <raphael.favre@cpnv.ch>
2020-03-04 09:33:24 +0100

    Ajout du message affichage aide

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main() {
     printf("Hello, World!\n");
-    printf("Bienvenue à la bataille navale!\n");
+    printf("Bienvenue a la bataille navale!\n");
+    printf("Appuyez sur 1 pour afficher l'aide\n");
 
     return 0;
 }
