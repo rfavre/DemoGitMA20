1e85175a01b086e5f51136975181343255a69331
raphael.favre <raphael.favre@cpnv.ch>
2020-03-06 14:45:10 +0100

    Nouvelle ligne depuis PC salle C133

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     printf("Bienvenue a la bataille navale!\n");
     printf("Appuyez sur 1 pour afficher l'aide\n");
     printf("Appuyez sur 2 pour jouer\n");
+    printf("demo ligne");
 
     return 0;
 }
